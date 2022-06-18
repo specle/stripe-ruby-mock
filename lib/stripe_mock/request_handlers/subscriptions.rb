ea246cc7314a85d217174368050d3d350aa20dc9
Mark Fairburn <mark@specle.net>
2022-06-18 09:22:29 +0100

    properly nest the rescue

--- lib/stripe_mock/request_handlers/subscriptions.rb
+++ lib/stripe_mock/request_handlers/subscriptions.rb
@@ -307,9 +307,11 @@ module StripeMock
                    end
         plan_ids.compact!
         plan_ids.each do |plan_id|
-          assert_existence :plan, plan_id, plans[plan_id]
-        rescue Stripe::InvalidRequestError
-          assert_existence :price, plan_id, prices[plan_id]
+          begin
+            assert_existence :plan, plan_id, plans[plan_id]
+          rescue Stripe::InvalidRequestError
+            assert_existence :price, plan_id, prices[plan_id]
+          end
         end
         plan_ids.map { |plan_id| plans[plan_id] || prices[plan_id]}
       end
